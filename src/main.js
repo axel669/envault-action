f9ab56eb667ef316b9236c082e36612ef7bc9bb4
axel669 <haryuu.hanekata@gmail.com>
2024-11-22 21:17:29 -0800

    switch to ncc so i can use modern scripting

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import core from "@actions/core"
 import yaml from "js-yaml"
 import fs from "node:fs/promises"
 
-const main = async () => {
-    core.exportVariable("envault_success", "true")
-    console.log(process.cwd())
-    console.log(
-        await fs.readdir(
-            process.cwd()
-        )
+core.exportVariable("envault_success", "true")
+console.log(process.cwd())
+console.log(
+    await fs.readdir(
+        process.cwd()
     )
-}
-main()
+)
